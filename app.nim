72b46bc6cf59efd6876f3b6ef4637b825b413f38
null-flame <hesam.flame@proton.me>
2026-07-04 19:10:56 +0330

    rate Limit in url

--- app.nim
+++ app.nim
@@ -4,17 +4,44 @@ import handlers/[posts, auth, actions]
 
 runData()
 
-Route.post("/api/reg", auth.reg)
-Route.post("/api/loginEmail", auth.loginEmail)
-Route.post("/api/view", posts.view)
+type
+    methods = enum
+        GET, POST, PUT, DELETE
 
-Route.get("/api/get_post", getList)
+proc rate(m: methods, int1, int2: int, url: string, handler: proc(ctx: Context) {.async.}) {.async.} =
+    if m == methods.GET:
+        Route.groupPath("/", rateLimit(int1, int2)):
+            Route.get(url, handler)
+    elif m == methods.POST:
+        Route.groupPath("/", rateLimit(int1, int2)):
+            Route.post(url, handler)
+    elif m == methods.PUT:
+        Route.groupPath("/", rateLimit(int1, int2)):
+            Route.put(url, handler)
+    elif m == methods.DELETE:
+        Route.groupPath("/", rateLimit(int1, int2)):
+            Route.delete(url, handler)
+
+
+discard rate(methods.POST, 2, 60, "/api/reg", reg)
+
+
+discard rate(methods.POST, 3, 60, "/api/loginEmail", loginEmail)
+
+
+discard rate(methods.POST, 10, 60, "/api/view", view)
+
+
+discard rate(methods.POST, 10, 60, "/api/get_post", getList)
+
 
 Route.groupPath("/api", guard):
-    Route.post("/create_post", posts.createPost)
-    Route.post("/like", actions.like)
     
-    Route.delete("/delete", deletePost)
+    discard rate(methods.POST, 5, 60, "/create_post", createPost)
+
+    discard rate(methods.POST, 10, 60, "/like", like)
+
+    discard rate(methods.DELETE, 3, 60, "/delete", deletePost)
 
 
 Jazzy.serve(8080)
