4dbe91cca030f1b8901c6480f6c7b751c3d74570
null-flame <hesam.flame@proton.me>
2026-07-05 18:30:23 +0330

    refactor(auth): decouple db logic from handlers using a service layer

--- models/user.nim
+++ models/user.nim
@@ -1,10 +1,12 @@
 import norm/model
 import std/times
+import norm/pragmas
+
 
 type
     User* = ref object of Model
-        name*: string
+        name*  {.unique.}: string
         pass*: string
-        email*: string
+        email* {.unique.}: string
         age*: int
         create_time*: DateTime
---
+++ services/auth_service.nim
@@ -0,0 +1,64 @@
+import jazzy except DbValue, dbValue, dbNull
+import norm/[sqlite, model]
+import std/times
+import std/strutils
+import ../models/user
+import ../config
+
+
+
+
+
+proc createToken*(ctx: Context, data: JsonNode): string=
+    
+    var user = User(email: data["email"].getStr())
+
+    db.select(user, "email = ?", user.email)
+
+    let exp = getTime().toUnix() + 2592000
+
+    let pay = %* {"name": user.name, "email": user.email, "age": user.age, "id": user.id, "exp": exp}
+
+    let token = ctx.login(pay, true)
+    
+    return token
+
+
+
+proc setUserInDataBase*(data: JsonNode)=
+    
+    let
+        name = data["name"].getStr
+        email = data["email"].getStr
+        pass = data["pass"].getStr
+        age = data["age"].getInt
+        create_time = now()
+
+    var user = User(name: name, email: email, pass: pass, age: age, create_time: create_time)
+
+    db.insert(user)
+
+
+proc checkTrue*(data: JsonNode): bool=
+
+    let
+        email = data["email"].getStr()
+        pass = data["pass"].getStr()
+    
+    let count = db.count(User, "*", false, "email = ?", email)
+
+    if count == 1:
+        var user = User()
+        db.select(user, "email = ?", email)
+        let passD = user.pass
+        
+        if passD == pass:
+            return true
+        
+        else: return false
+
+    else: return false
+
+
+
+
