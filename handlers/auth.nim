171beb8637f5b1d28bae6866ad24fa1508100bf9
null-flame <hesam.flame@proton.me>
2026-07-05 18:30:23 +0330

    refactor(auth): decouple db logic from handlers using a service layer

--- handlers/auth.nim
+++ handlers/auth.nim
@@ -4,6 +4,8 @@ import std/times
 import std/strutils
 import ../models/user
 import ../config
+import ../services/auth_service
+
 
 proc reg*(ctx: Context) {.async.}=
     let data = ctx.validate(%*{"name": "required|min:4",
@@ -11,51 +13,25 @@ proc reg*(ctx: Context) {.async.}=
     "email": "required|min:4",
     "age": "required|integer"
     })
-
-    var
-        name = data["name"].getStr()
-        pass = data["pass"].getStr()
-        hashedpassword = hashPassword(pass)
-        email = data["email"].getStr()
-        age = data["age"].getInt()
-        create_time = now()
-        user = User(name: name, pass: hashedpassword, age: age, create_time: create_time, email: email)
     
     try:
-        db.insert(user)
+        setUserInDataBase(data)
     except:
-        ctx.json(%*{"ss": "400", "msg": "This name or email is already in use."})
+        ctx.status(400).json(%*{"ss": "400", "msg": "This name or email is already in use."})
         return
-    let expireTime = getTime().toUnix() + 2592000
 
-    db.select(user, "name = ? AND pass = ? AND email = ?", name, hashedpassword, email)
-
-    let pay = %* {"name": name, "email": email, "age": age, "id": user.id, "exp": expireTime}
-
-    let token = ctx.login(pay, true)
+    let token = ctx.createToken(data)
     
     ctx.status(201).json(%*{"ss": "ok", "token": token})
 
 proc loginEmail*(ctx: Context) {.async.}=
     var data = ctx.validate(%*{"email": "required|email", "pass": "required|min:7"})
-    try:
-        var
-            email = data["email"].getStr()
-            pass = data["pass"].getStr()
-            u = User()
-        db.select(u, "email = ?", email)
 
-        if verifyPassword(pass, u.pass):
-            var
-                email = u.email
-                age = u.age
-                name = u.name
-            let
-                exp = getTime().toUnix() + 2592000
-                pay = %*{"name": name, "email": email, "age": age, "id": u.id, "exp": exp}
-                token = ctx.login(pay, true)
-            ctx.json(%*{"ss": 200, "token": token})
-        else:
-            ctx.json(%*{"ss": "400", "msg": "The email or password is incorrect."})
-    except NotFoundError:
+    if checkTrue(data):
+
+        let token = ctx.createToken(data)
+        
+        ctx.json(%*{"ss": "ok", "token": token})
+
+    else:
         ctx.json(%*{"ss": "400", "msg": "The email or password is incorrect."})
